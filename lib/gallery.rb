f27048fcb8069ea8b83abf3389cbb0f4af47070e
Callum Forbes <forbescallum@gmail.com>
2019-08-24 17:51:37 +0100

    Add #add_search_terms method to Gallery with tests

--- lib/gallery.rb
+++ lib/gallery.rb
@@ -2,4 +2,11 @@ $API_PARAMS = %w[q lang id image_type orientation category min_width min_height 
 
 class Gallery
   attr_reader(*$API_PARAMS.map(&:to_sym))
+  def initialize
+    $API_PARAMS.each { |param| instance_variable_set('@'+param, '')}
+  end
+
+  def add_search_terms(*terms)
+    @q = (@q.empty? ? terms : [@q] + terms).join('+')
+  end
 end
--- spec/gallery_spec.rb
+++ spec/gallery_spec.rb
@@ -3,11 +3,42 @@ require_relative '../lib/gallery.rb'
 
 
 describe 'Creating a Gallery' do
-  describe 'GIVEN the Gallery class, some criteria, and a Pixabay object' do
+  describe 'GIVEN the Gallery class and an API params constant for pixabay.com' do
     describe 'WHEN Gallery#new is called with no arguments' do
       it "THEN an object is created which has attributes Pixabay's API exposes" do
         gallery = Gallery.new
         expect(gallery).to respond_to(*$API_PARAMS.map(&:to_sym))
+      end
+    end
+  end
+end
+
+describe 'Configuring a Gallery' do
+  before :each do
+    @gallery = Gallery.new
+  end
+
+  describe 'GIVEN some search terms' do
+    describe 'WHEN #add_search_terms is called with a single term' do
+      it 'THEN it sets #q to that term' do
+        @gallery.add_search_terms('foo')
+        expect(@gallery.q).to eq('foo')
+      end
+    end
+
+    describe 'WHEN #add_search_terms is called with a single term multiple times' do
+      it 'THEN it sets #q to those terms joined with "+"' do
+        @gallery.add_search_terms('foo')
+        @gallery.add_search_terms('bar')
+        @gallery.add_search_terms('baz')
+        expect(@gallery.q).to eq('foo+bar+baz')
+      end
+    end
+
+    describe 'WHEN #add_search_terms is called with multiple terms' do
+      it 'THEN it sets #q to those terms joined with "+"' do
+        @gallery.add_search_terms('foo', 'bar', 'baz')
+        expect(@gallery.q).to eq('foo+bar+baz')
       end
     end
   end
